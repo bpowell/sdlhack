6e5859ed840049b6ae8a3befcfd109dd59f08ac4
Brandon Powell <bpowell29a@gmail.com>
2013-12-19 18:16:43 -0500

    Init commit of code.

---
+++ hack.cpp
@@ -0,0 +1,47 @@
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+#include <iostream>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+class Game{
+	private:
+		SDL_Window *window;
+	public:
+		bool valid;
+		Game(){
+			if(SDL_Init(SDL_INIT_VIDEO) < 0){
+				valid = false;
+				return;
+			}
+
+			if(!(SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1"))){
+				std::cout << "VSYNC not enabled." << std::endl;
+			}
+
+			if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY,"1")){
+				std::cout << "Linear texture filtering not enabled." << std::endl;
+			}
+
+			window = SDL_CreateWindow( "Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+			if(window==NULL){
+				valid = false;
+				return;
+			}
+
+			valid = true;
+		}
+};
+
+int main(int argc, char *argv[]){
+	Game *game = new Game();
+	if(game->valid==false){
+		std::cout << "SDL failed to initialize." << std::endl;
+		std::cout << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
