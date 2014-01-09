072102a4084c397192dc20baaac8ffae7f4235ae
Brandon Powell <bpowell29a@gmail.com>
2014-01-09 17:28:34 -0500

    Sprite constructor.

--- Sprite.cpp
+++ Sprite.cpp
@@ -1,6 +1,31 @@
 #include "Sprite.h"
 #include <iostream>
 
+Sprite::Sprite(SDL_Renderer *renderer, std::string path, Vector2D location, Vector2D size, int default_clip) :
+	renderer(renderer), location(location), size(size), default_clip(default_clip)
+{
+		texture = NULL;
+		SDL_Surface *surface = IMG_Load(path.c_str());
+
+		if(surface==NULL){
+			std::cerr << "Cannot load image " << path << std::endl;
+			return;
+		}
+
+		if(default_clip<0){
+			std::cerr << "Cannot load clip from image " << path << std::endl;
+			return;
+		}
+
+		texture = SDL_CreateTextureFromSurface(renderer, surface);
+		if(texture==NULL){
+			std::cerr << "Cannot load image " << path << std::endl;
+			return;
+		}
+
+		SDL_FreeSurface(surface);
+}
+
 SpriteManager::SpriteManager(){}
 
 SpriteManager::~SpriteManager(){
